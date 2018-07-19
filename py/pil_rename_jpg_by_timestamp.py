dcec852974512006d2f244a5a2b10823bcbc7796
xiarendeniao <xudongsong0102@163.com>
2018-07-19 14:46:42 +0800

    Update pil_rename_jpg_by_timestamp.py

--- py/pil_rename_jpg_by_timestamp.py
+++ py/pil_rename_jpg_by_timestamp.py
@@ -1,7 +1,7 @@
 #encoding=utf-8
 
 from PIL import Image
-import os
+import os, platform
 
 def Deal(fname):
     try:
@@ -16,13 +16,20 @@ def Deal(fname):
         #print '%s ignored %r' % (fname, e)  #png
         return
 
+    m.fp.close()
+
     if type(info) == dict and 36867 in info:
         dt = info[36867]
         dt = dt.replace(':','').replace(' ','-')
         #print '%s timestamp %s' % (fname, dt)
         right_index = fname.rfind('.')
         assert(right_index > 0)
-        left_index = fname.rfind('/')
+        
+        if platform.uname()[0] == 'Windows':
+            left_index = fname.rfind('\\')
+        else:
+            left_index = fname.rfind('/')
+
         name = '%s%s%s' % (left_index >= 0 and fname[0:left_index+1] or '', dt, fname[right_index:])
 
         if name == fname:
@@ -34,6 +41,7 @@ def Deal(fname):
             name = '%s%s-%d%s' % (left_index >= 0 and fname[0:left_index+1] or '', dt, index, fname[right_index:])
             index += 1
 
+        #import pdb; pdb.set_trace()
         print 'rename %s -> %s' % (fname, name)
         os.rename(fname, name)
 

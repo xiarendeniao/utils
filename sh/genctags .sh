36eb7ca4e6aa32704608ee9df1325dd7c66f8e25
xiarendeniao <xudongsong0102@163.com>
2018-10-15 12:58:35 +0800

    Update genctags .sh
    function

--- sh/genctags .sh
+++ sh/genctags .sh
@@ -1,15 +1,91 @@
-FNAME=/tmp/tags_to_file
+#!/bin/sh
 
-find include server libsrc  ../server_thirdparty_proj/tsf4g_base/include/  ../server_thirdparty_proj/tcaplus_service/include/ -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $FNAME && ctags --extra=f --c-types=+px -fc_include_declare_tags -L $FNAME 
+#Author                : dsxu
+#Version               :
+#Date Of Creation      : 2018-10-15
 
-find server include libsrc  ../server_thirdparty_proj/tsf4g_base/include/ ../server_thirdparty_proj/tcaplus_service/include/ -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $FNAME && ctags --extra=f -ftags -L $FNAME
+# params:  code_dir_list ctags_file ctags_params 
+CreateCTag()
+{
+    if [[ $# -lt 2 ]]
+    then
+        echo "invalid params "$*
+        exit 1
+    fi
 
-find include server/zonesvrd libsrc  ../server_thirdparty_proj/tsf4g_base/include/  ../server_thirdparty_proj/tcaplus_service/include/ -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $FNAME && ctags --extra=f --c-types=+px -fc_include_declare_tags_zone -L $FNAME
+    code_dir_list=$1
+    ctags_file=$2
+    ctags_params="--extra=f"
+    if [[ $# -ge 3 ]] 
+    then
+        ctags_params="$ctags_params $3"
+    fi
+    tmp_file=/tmp/c_tags_to_file
 
-find include server/zonesvrd libsrc  ../server_thirdparty_proj/tsf4g_base/include/ ../server_thirdparty_proj/tcaplus_service/include/ -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $FNAME && ctags --extra=f -ftags_zone -L $FNAME
+    echo $ctags_params
 
-#find include server libsrc  ../server_thirdparty_proj/ -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $FNAME && ctags --extra=f --c-types=+px -fc_include_third_tags -L $FNAME
+    find $code_dir_list -name "*.c" -or -name "*.h" -or -name "*.cpp" -or -name "*.cc" > $tmp_file && ctags $ctags_params -L $tmp_file -f $ctags_file
+
+    if [[ $? == 0 ]]
+    then
+        echo "ctags \"$code_dir_list\" \"$ctags_params\" > \"$ctags_file\" succ"
+    else
+        echo "ctags \"$code_dir_list\" \"$ctags_params\" > \"$ctags_file\" failed"
+    fi
+}
+
+# params:  code_dir_list ctags_file
+CreateLuaTag()
+{
+    if [[ $# -lt 2 ]]
+    then
+        echo "invalid params "$*
+        exit 1
+    fi
+
+    ctags_params='--langdef=MYLUA --langmap=MYLUA:.lua'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*(\w+)\s*=\s*class\s*\(.*$/\1/f/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^.*\w+\.(\w+)\s*=\s*function\s*\(.*$/\1/f/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^.*\s*function\s*(\w+):(\w+).*$/\2/f/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*(\w+)\s*=\s*[0-9]+.*$/\1/e/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^.*\s*function\s*(\w+)\.(\w+).*$/\2/f/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^.*\s*function\s*(\w+)\s*\(.*$/\1/f/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*(\w+)\s*=\s*\{.*$/\1/e/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*module\s+\"(\w+)\".*$/\1/m,module/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*module\s+\"[a-zA-Z0-9._]+\.(\w+)\".*$/\1/m,module/"'
+    ctags_params=$ctags_params' --regex-MYLUA="/^\s*function\s*(\w+):initialize\s*\(.*$/\1/f/"'
+    ctags_params=$ctags_params' --languages=MYLUA --excmd=pattern --extra=f'
+
+    code_dir_list=$1
+    ctags_file=$2
+    tmp_file=/tmp/lua_tags_to_file
+
+    find $code_dir_list -name "*.lua" > $tmp_file && ctags "$ctags_params" -L $tmp_file -f $ctags_file
+
+    if [[ $? == 0 ]]
+    then
+        echo "ctags \"$code_dir_list\" \"$ctags_params\" > \"$ctags_file\" succ"
+    else
+        echo "ctags \"$code_dir_list\" \"$ctags_params\" > \"$ctags_file\" failed"
+    fi
+}
+
+CreateCTag "server" tags
+
+CreateCTag "include server libsrc" tags2
+
+CreateCTag "include server libsrc tools" tags3
+
+CreateCTag "include server libsrc tools" tags4 "--c-types=+px"
+
+CreateCTag "include server libsrc tools ext" tags5 "--c-types=+px"
+
+CreateCTag "include server libsrc ../server_thirdparty_proj/behaviac-3.5.7/" aitags "--c-types=+px"
+
+CreateLuaTag "dataconfig/lua server/lua_conn server/lua_crontab server/lua_script"  luatags
 
 #tree -asfDF server > fs.server
 
 tree -af server > fs.server
+
+tree -af . > fs
